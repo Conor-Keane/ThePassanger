4e5aead16278dd3a0c32337f91c3665fcfa8d86e
conor-keane <g00391846@atu.ie>
2022-10-26 12:23:57 +0100

    Added a test for title

--- src/test/java/ie/atu/week6/PassengerTest.java
+++ src/test/java/ie/atu/week6/PassengerTest.java
@@ -2,11 +2,20 @@ package ie.atu.week6;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PassengerTest {
 
+    Passenger myPass;
+
     @BeforeEach
     void setUp() {
+        myPass = new Passenger();
+    }
+
+    @Test
+    void testTitle() {
+
     }
 
     @AfterEach
