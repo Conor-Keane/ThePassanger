0724f29c3ae1a7a85642348a7d71277ee04646b9
conor-keane <g00391846@atu.ie>
2022-10-26 14:13:43 +0100

    Added a constructor and setter

--- src/main/java/ie/atu/week6/Passenger.java
+++ src/main/java/ie/atu/week6/Passenger.java
@@ -1,4 +1,39 @@
 package ie.atu.week6;
 
 public class Passenger {
+    private String Title;
+    private String Name;
+    private String Id;
+    private int Phone_no;
+    private int Age;
+
+    public Passenger(String title, String name, String id, int phone_no, int age) {
+        Title = title;
+        Name = name;
+        Id = id;
+        Phone_no = phone_no;
+        Age = age;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public void setPhone_no(int phone_no) {
+        Phone_no = phone_no;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+
 }
--- src/test/java/ie/atu/week6/PassengerTest.java
+++ src/test/java/ie/atu/week6/PassengerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class PassengerTest {
 
     Passenger myPass;
@@ -15,7 +17,7 @@ class PassengerTest {
 
     @Test
     void testTitle() {
-
+        assertEquals();
     }
 
     @AfterEach
